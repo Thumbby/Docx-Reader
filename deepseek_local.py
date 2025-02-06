d9ceb2634ed264074406d273839f1600429fb0e5
Thumbby <254091042@qq.com>
2025-02-06 16:26:03 +0800

    add initial prompt

--- deepseek_local.py
+++ deepseek_local.py
@@ -15,10 +15,18 @@ def get_response(user_input: str, messages: list = []):
     )
     
     print(response.message.content)
+    
+    messages.append({
+        'role': 'assistant',
+        'content': response.message.content
+    })
 
 if __name__ == "__main__":
     # initialize the memory
-    messages = []
+    messages = [{
+        'role':'system',
+        'content': '你是一个仿生人,可以模仿人类的情感并且表达在回答中,请根据回答的内容选择适合的语气进行'
+    }]
     while True:
         user_input = input("请输入对话:")
         get_response(user_input, messages=messages)
